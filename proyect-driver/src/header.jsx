4c090e91b8f4a1f87e656ef7d1d0b67f45ff165f
Yamil <yamilgiron98@gmail.com>
2022-12-20 15:29:20 -0300

    20/12

--- proyect-driver/src/header.jsx
+++ proyect-driver/src/header.jsx
@@ -2,13 +2,12 @@ import "./header.css"
 
 export default function header(){
     return  <header className="headerContent">
-                <img src="/proyect-driver/recursos/img.jpg" className="imagen"></img>
+                <img src="C:\Users\Yamil\Documents\Proyect_driver\proyect-driver\recursos\img.jpg" className="logo"></img>
                 <h1 className="titulo">Driver Proyect</h1>
-                <form>
+                <form className="ingresar">
                     <input type="text" />
-                    <input type="text" />
+                    <input type="password" />
                     <button className="button"> Ingresar </button>
-                    <button className="button"> Registrarse </button>
                 </form>
             </header>
 }
